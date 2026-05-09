867a8cfdc2af8d5f819826fdabcdb62716214af0
Geniacc <geniacc@example.com>
2026-05-09 17:29:25 +0530

    fix: adjust base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // This tells Vite your site is at /urja-mobility/ instead of the root
+  base: '/urja-mobility/',
   plugins: [react()],
   optimizeDeps: {
     include: ['react-simple-maps', 'react-tooltip', 'prop-types']
